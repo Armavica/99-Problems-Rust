6dfd7f1e5a8fcc18de64c533a4dca96fb7307349
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-25 17:54:15 +0100

    Redo Problem 04 with vectors

--- 1-10/p04.rs
+++ 1-10/p04.rs
@@ -1,16 +1,7 @@
-enum List<T> {
-    Cons(T, ~List<T>),
-    Nil
-}
-
-fn length<T>(list: &List<T>) -> uint {
-    match *list {
-        Nil => 0,
-        Cons(_, ~ref rest) => length(rest)+1
-    }
-}
-
 fn main() {
-    let list: List<char> = Cons('a', ~Cons('b', ~Cons('c', ~Cons('d', ~Nil))));
-    println!("{}", length(&list));
+    let list = ~['a', 'b', 'c'];
+    println!("{}", list.len());
+
+    let list: ~[uint] = ~[];
+    println!("{}", list.len());
 }
