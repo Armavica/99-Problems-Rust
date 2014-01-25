5a741e2f144b7441e781b81d929dbebed59fde7e
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-26 00:39:04 +0100

    Redo Problem 07 with vectors

--- 1-10/p07.rs
+++ 1-10/p07.rs
@@ -1,41 +1,19 @@
-enum NestedList<T> {
-    Nil,
-    One(T, ~NestedList<T>),
-    Many(~NestedList<T>, ~NestedList<T>)
+enum Node<T> {
+    One(T),
+    Many(~[Node<T>])
 }
 
-fn rev<T>(list: NestedList<T>) -> NestedList<T> {
-    fn rev_aux<T>(list: NestedList<T>, acc: NestedList<T>) -> NestedList<T> {
-        match list {
-            Nil => acc,
-            One(elem, ~rest) => rev_aux(rest, One(elem, ~acc)),
-            Many(~node, ~rest) => rev_aux(rest, Many(~rev(node), ~acc))
-        }
-    }
-    rev_aux(list, Nil)
+fn flatten<T: Clone>(nlist: &~[Node<T>]) -> ~[T] {
+    nlist.flat_map(|n|
+                   match *n {
+                       One(ref e) => ~[e.clone()],
+                       Many(ref el) => flatten(el)
+                   } )
 }
-
-fn flatten<T: Clone>(list: &NestedList<T>) -> NestedList<T> {
-    fn aux<T: Clone>(list: &NestedList<T>, acc: NestedList<T>) -> NestedList<T> {
-        match *list {
-            Nil => acc,
-            One(ref elem, ~ref rest) => aux(rest, One(elem.clone(), ~acc)),
-            Many(~ref node, ~ref rest) => aux(rest, aux(node, acc))
-        }
-    }
-
-    rev(aux(list, Nil))
-}
-
 
 fn main() {
-    let list: NestedList<char> =
-        One('a', ~Many(
-                ~One('b', ~Many(
-                        ~One('c', ~One('d', ~Nil)),
-                        ~One('e', ~Nil)
-                    )),
-            ~Nil));
+    let nlist =
+        ~[One('a'), Many(~[One('b'), Many(~[One('c'), One('d')]), One('e')])];
 
-    println!("{:?}", flatten(&list));
+    println!("{:?}", flatten(&nlist));
 }
