bc51da8a38ff1b0a9eef7f25c7e54155d33096bb
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-30 15:42:29 +0100

    Clean up Problem 03

--- 1-10/p03.rs
+++ 1-10/p03.rs
@@ -1,4 +1,4 @@
-fn kth<'a, T>(k:uint, list: &'a ~[T]) -> Option<&'a T> {
+fn kth<'a, T>(k:uint, list: &'a [T]) -> Option<&'a T> {
     if list.len() >= k && k > 0{
         Some(&list[k-1])
     } else {
@@ -8,8 +8,8 @@ fn kth<'a, T>(k:uint, list: &'a ~[T]) -> Option<&'a T> {
 
 fn main() {
     let list = ~['a', 'b', 'c', 'd', 'e'];
-    println!("{:?}", kth(3, &list));
+    println!("{:?}", kth(3, list));
 
     let list = ~['a'];
-    println!("{:?}", kth(3, &list));
+    println!("{:?}", kth(3, list));
 }
