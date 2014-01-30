d6123c60395c2b803c40e04eae15c09b4948f5ea
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-30 01:21:43 +0100

    Do Problem 01 without cheating

--- 1-10/p01.rs
+++ 1-10/p01.rs
@@ -1,4 +1,12 @@
+fn last<'a, T>(list: &'a [T]) -> Option<&'a T> {
+    match list {
+        [] => None,
+        [.., ref x] => Some(x)
+    }
+}
+
 fn main() {
     let list: ~[char] = ~['a', 'b', 'c'];
-    println!("{:?}", list.last());
+    println!("{:?}", last(list));
+    println!("{:?}", list.last()); // Cheat
 }
