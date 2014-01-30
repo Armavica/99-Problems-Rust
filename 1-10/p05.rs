a6ffea6f1210b86545f37b3af6be3abe4b52c69d
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-31 00:43:12 +0100

    Redo Problem 05 with cheat

--- 1-10/p05.rs
+++ 1-10/p05.rs
@@ -1,12 +1,5 @@
-fn rev<T: Clone>(list: &~[T]) -> ~[T] {
-    let mut tsli = ~[];
-    for elem in list.iter() {
-        tsli.unshift(elem.clone());
-    }
-    tsli
-}
-
 fn main() {
     let list = ~['a', 'b', 'c', 'd', 'e'];
-    println!("{:?}", rev(&list));
+    let l:~[&char] = list.rev_iter().collect(); // Cheat
+    println!("{:?}", l);
 }
