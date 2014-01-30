f79043181a0c9ab5588c6aa769fbf8a89f3dbcbe
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-01-30 01:56:21 +0100

    Redo Problem 02 with pattern matching

--- 1-10/p02.rs
+++ 1-10/p02.rs
@@ -1,15 +1,14 @@
-fn lastbutone<'a, T>(list: &'a ~[T]) -> Option<(&'a T, &'a T)> {
-    if list.len() > 2 {
-        Some((&list[list.len()-2], &list[list.len()-1]))
-    } else {
-        None
+fn lastbutone<'a, T>(list: &'a [T]) -> Option<(&'a T, &'a T)> {
+    match list {
+        [] | [_] => None,
+        [.., ref x, ref y] => Some((x, y))
     }
 }
 
 fn main() {
     let list = ~['a', 'b', 'c', 'd', 'e'];
-    println!("{:?}", lastbutone(&list));
+    println!("{:?}", lastbutone(list));
 
     let list = ~['a'];
-    println!("{:?}", lastbutone(&list));
+    println!("{:?}", lastbutone(list));
 }
