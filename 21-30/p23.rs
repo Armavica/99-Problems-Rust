dbd5092951f833e7419e270234de694d1f3bef65
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-02-01 23:29:43 +0100

    Problem 23

---
+++ 21-30/p23.rs
@@ -0,0 +1,11 @@
+use std::rand::{task_rng, Rng};
+
+fn rnd_select<'a, T: Clone>(list: &'a [T], n: uint) -> ~[&'a T] {
+    task_rng().sample(list.iter(), n)
+}
+
+fn main() {
+    let list = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    println!("{:?}", rnd_select(list, 3));
+}
+
