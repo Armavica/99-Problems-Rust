41d1505e04e94805358fea964943b7604771654f
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-03-12 00:41:54 +0100

    Problem 63

---
+++ 61-70/p63.rs
@@ -0,0 +1,40 @@
+enum BinaryTree<T> {
+    Node(T, ~BinaryTree<T>, ~BinaryTree<T>),
+    Empty
+}
+
+impl<T: Eq> Eq for BinaryTree<T> {
+    fn eq(&self, other: &BinaryTree<T>) -> bool {
+        match (self, other) {
+            (&Empty, &Empty) => true,
+            (&Node(ref lv, ~ref ll, ~ref lr),
+             &Node(ref rv, ~ref rl, ~ref rr)) => lv == rv &&
+                                                 ll == rl &&
+                                                 lr == rr,
+            _ => false
+        }
+    }
+}
+
+fn complete_binary_tree(n: uint) -> BinaryTree<uint> {
+    fn aux(start: uint, n: uint) -> BinaryTree<uint> {
+        if start > n {
+            Empty
+        } else if 2*start + 1 > n {
+            Node(start, ~aux(2*start, n),
+                        ~Empty)
+        } else {
+            Node(start, ~aux(2*start, n),
+                        ~aux(2*start+1, n))
+        }
+    }
+    aux(1, n)
+}
+
+fn main() {
+    assert!(complete_binary_tree(6) == Node(1, ~Node(2, ~Node(4, ~Empty, ~Empty),
+                                                        ~Node(5, ~Empty, ~Empty)),
+                                               ~Node(3, ~Node(6, ~Empty, ~Empty),
+                                                        ~Empty)));
+}
+
