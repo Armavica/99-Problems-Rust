9167e47053d82c363121f5ba1282375d30034e24
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-21 22:20:25 +0200

    Cleanup Problem 14

--- 11-20/p14.rs
+++ 11-20/p14.rs
@@ -1,14 +1,21 @@
-fn dupli<T: Clone>(list: &[T]) -> ~[T] {
-    let mut r = ~[];
-    for e in list.iter() {
-        r.push(e.clone());
-        r.push(e.clone());
-    }
-    r
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
+
+//! Problem 14: Vectors: duplicate
+//!
+//! Duplicate the elements of a vector.
+//!
+//! Your function could have this signature:
+//! `fn dupli<T: Clone>(vec: ~[T]) -> ~[T]`
+
+fn dupli<T: Clone>(vec: ~[T]) -> ~[T] {
+    vec.move_iter().flat_map(|e| Vec::from_elem(2, e).move_iter()).collect()
 }
 
-fn main() {
-    let list = ~['a', 'b', 'c', 'c', 'd'];
-    println!("{:?}", dupli(list));
+#[test]
+fn dupli_test() {
+    let vec = ~['a', 'b', 'c', 'c', 'd'];
+    assert_eq!(dupli(vec), ~['a', 'a', 'b', 'b', 'c', 'c', 'c', 'c', 'd', 'd']);
 }
-
