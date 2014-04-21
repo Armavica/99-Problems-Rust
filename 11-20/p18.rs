b68b9a6b348349cf19f58ce12b6ae1de078154f1
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-21 22:50:56 +0200

    Cleanup Problem 18

--- 11-20/p18.rs
+++ 11-20/p18.rs
@@ -1,10 +1,33 @@
-fn slice<'a, T>(list: &'a [T], i: uint, k: uint) -> &'a [T] {
-    list.slice(i-1, k)
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
+
+//! Problem 18: Vectors: slice
+//!
+//! Given two indices, `i` and `k`, the slice is the vector containing the
+//! elements between the `i`th and `k`th element of the original vector (first
+//! limit included only).  Start counting the elements with 0.  Your function
+//! should not fail in case one or the other indices point outside the vector.
+//!
+//! Your function could have this signature:
+//! `fn dupli<T>(vec: &'a [T], i: uint, k: uint) -> &'a [T]`
+
+use std::cmp::min;
+
+fn slice<'a, T>(vec: &'a [T], i: uint, k: uint) -> &'a [T] {
+    vec.slice(min(i, vec.len()), min(k, vec.len()))
 }
 
-fn main() {
-    let list = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    println!("{:?}", slice(list, 3, 7));
-    println!("{:?}", slice(list, 3, 7));
+#[test]
+fn slice_ok() {
+    let vec = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    assert_eq!(slice(vec, 3, 7), &['d', 'e', 'f', 'g']);
+}
+
+#[test]
+fn slice_outside() {
+    let vec = ~['a', 'b', 'c', 'd', 'e'];
+    assert_eq!(slice(vec, 7, 10), &[]);
 }
 
