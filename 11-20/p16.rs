7f0b55950894c673118a4587f54f486f04a9c074
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-21 22:28:46 +0200

    Cleanup Problem 16

--- 11-20/p16.rs
+++ 11-20/p16.rs
@@ -1,16 +1,29 @@
-fn drop<T: Clone>(list: &[T], n: uint) -> ~[T] {
-    list.iter().enumerate().filter_map(
-        |(i,&ref e)|
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
+
+//! Problem 16: Vectors: drop
+//!
+//! Drop every `n`th element of a vector.
+//!
+//! Your function could have this signature:
+//! `fn drop<T>(n: uint, vec: ~[T]) -> ~[T]`
+
+fn drop<T>(vec: ~[T], n: uint) -> ~[T] {
+    vec.move_iter().enumerate().filter_map(
+        |(i,e)|
             if i%n < n-1 {
-                Some(e.clone())
+                Some(e)
             } else {
                 None
             }
         ).collect()
 }
 
-fn main() {
-    let list = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
-    println!("{:?}", drop(list, 3));
+#[test]
+fn drop_test() {
+    let vec = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+    assert_eq!(drop(vec, 3), ~['a', 'b', 'd', 'e', 'g', 'h', 'j']);
 }
 
