daa72a985e1e6d1724955a0ccdc5523d9f946116
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-22 02:29:59 +0200

    Handle a cornercase in Problem 17
    Gracefully handle out of bound arguments.

--- 11-20/p17.rs
+++ 11-20/p17.rs
@@ -6,12 +6,16 @@
 //! Problem 17: Vectors: split
 //!
 //! Split a vector into two parts; the length of the first part is given.
+//! If the length of the first part is longer than the entire vec, then the
+//! first part is the vec and the second part is empty.
 //!
 //! Your function could have this signature:
 //! `fn split<T>(vec: &[T], n: uint) -> (&[T], &[T])`
 
+use std::cmp::min;
+
 fn split<'a, T>(vec: &'a [T], n: uint) -> (&'a [T], &'a [T]) {
-    (vec.slice_to(n), vec.slice_from(n))
+    (vec.slice_to(min(n, vec.len())), vec.slice_from(min(n, vec.len())))
 }
 
 #[test]
@@ -32,5 +36,12 @@ fn split_begin() {
 fn split_end() {
     let vec = ['a', 'b', 'c', 'd', 'e'];
     assert_eq!(split(vec, 5),
+        (&['a', 'b', 'c', 'd', 'e'], &[]));
+}
+
+#[test]
+fn split_longer() {
+    let vec = ['a', 'b', 'c', 'd', 'e'];
+    assert_eq!(split(vec, 10),
         (&['a', 'b', 'c', 'd', 'e'], &[]));
 }
