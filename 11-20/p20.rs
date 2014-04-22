b037b962b78f3ceb8ff51dad596548d7105c02fc
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-22 02:33:18 +0200

    Cleanup Problem 20

--- 11-20/p20.rs
+++ 11-20/p20.rs
@@ -1,12 +1,30 @@
-fn drop<T: Clone>(list: &[T], k: uint) -> ~[T] {
-    let mut res = ~[];
-    res.push_all(list.slice_to(k-1));
-    res.push_all(list.slice_from(k));
-    res
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
+
+//! Problem 20: Vectors: remove_at
+//!
+//! Return a vector with its `k`th element removed.
+//!
+//! Your function could have this signature:
+//! `fn remove_at<T>(vec: ~[T], k: uint) -> ~[T]`
+
+fn remove_at<T>(vec: ~[T], k: uint) -> ~[T] {
+    vec.move_iter()
+       .enumerate()
+       .filter_map(|(n, e)| if n != k {Some(e)} else {None})
+       .collect()
 }
 
-fn main() {
+#[test]
+fn remove_at_in() {
     let list = ~['a', 'b', 'c', 'd'];
-    println!("{:?}", drop(list, 2));
+    assert_eq!(remove_at(list, 2), ~['a', 'b', 'd']);
 }
 
+#[test]
+fn remove_at_out() {
+    let list = ~['a', 'b', 'c', 'd'];
+    assert_eq!(remove_at(list, 10), ~['a', 'b', 'c', 'd']);
+}
