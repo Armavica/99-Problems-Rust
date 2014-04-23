ca44d8f55f6a7e00fbfac8b95920e75e26a74dab
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-23 03:07:50 +0200

    Cleanup Problem 24

--- 21-30/p24.rs
+++ 21-30/p24.rs
@@ -1,10 +1,18 @@
-use std::rand::{task_rng, Rng};
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
 
-fn lotto_select(n: uint, m: uint) -> ~[uint] {
-    task_rng().sample(range(1, m+1), n)
+//! Problem 24: Vectors: lotto_select
+//!
+//! Lotto: Draw `k` different random numbers from the set `0..n-1`.
+//!
+//! Your function could have this signature:
+//! `fn lotto_select(k: uint, n: uint) -> Vec<uint>`
+
+extern crate rand;
+use rand::{task_rng, Rng};
+
+fn lotto_select(k: uint, n: uint) -> Vec<uint> {
+    task_rng().sample(range(0, n), k)
 }
-
-fn main() {
-    println!("{:?}", lotto_select(6, 49));
-}
-
