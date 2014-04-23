c3b87fdc171309a4620369844cfadcc995f0fa81
Virgile Andreani <virgile.andreani@anbuco.fr>
2014-04-23 03:02:19 +0200

    Cleanup Problem 23

--- 21-30/p23.rs
+++ 21-30/p23.rs
@@ -1,11 +1,19 @@
-use std::rand::{task_rng, Rng};
+// The author of this work hereby waives all claim of copyright (economic and
+// moral) in this work and immediately places it in the public domain; it may
+// be used, distorted or destroyed in any manner whatsoever without further
+// attribution or notice to the creator.
 
-fn rnd_select<'a, T>(list: &'a [T], n: uint) -> ~[&'a T] {
-    task_rng().sample(list.iter(), n)
+//! Problem 23: Vectors: rnd_select
+//!
+//! Extract a given number of randomly selected elements from a vector.
+//! The selected items shall be returned in a vector.
+//!
+//! Your function could have this signature:
+//! `fn rnd_select<'a, T>(vec: &'a [T], n: uint) -> Vec<&'a T>`
+
+extern crate rand;
+use rand::{task_rng, Rng};
+
+fn rnd_select<'a, T>(vec: &'a [T], n: uint) -> Vec<&'a T> {
+    task_rng().sample(vec.iter(), n)
 }
-
-fn main() {
-    let list = ~['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-    println!("{:?}", rnd_select(list, 3));
-}
-
